e7f6d02e9d2e9c8d2cb89046b58c8748a17b7ca0
Matt Haechrel <haechrelmatt@gmail.com>
2020-04-21 19:52:17 -0500

    Added concate_images

---
+++ concat_images.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+
+def concat_images(image1, image2):
+    """
+    Concatenate two images with the same height or width. The resulting image will have the color mode of `image1`.
+    :param image1: The left or top image, depending on if concatenating horizontally or vertically.
+    :param image2: The right or bottom image
+    :return: concatenated Pillow Image
+    """
+    mode = image1.mode
+    if image1.width == image2.width:
+        new_image = Image.new(mode, (image1.width, image1.height + image2.height))
+        new_image.paste(image1, (0, 0))
+        new_image.paste(image2, (0, image1.height))
+    elif image1.height == image2.height:
+        new_image = Image.new(mode, (image1.width + image2.width, image1.height))
+        new_image.paste(image1, (0, 0))
+        new_image.paste(image2, (image1.width, 0))
+    else:
+        raise Exception("Images do not have the same height or width.")
+
+    return new_image
