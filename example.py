b89b15d59d55de3bb30b61c9db5d45957f76471f
Matt Haechrel <haechrelmatt@gmail.com>
2020-05-14 19:04:15 -0500

    Added example

---
+++ example.py
@@ -0,0 +1,39 @@
+import os
+
+from PIL import Image
+
+import print_cards
+
+path_to_images = './images'  # Replace this with the path to the directory containing your images.
+output_directory = './output'  # Replace this with the directory where you want the modified images to be saved.
+
+# Loop through image files in the directory
+for file in os.listdir(path_to_images):
+    if file.endswith(('.jpg', '.png', '.tif')):
+        image = Image.open(os.path.join(path_to_images, file))
+
+        # Remove IDC layers
+        image = print_cards.invert_idc_layers(image)
+
+        # Cover corners
+        if image.width > image.height:
+            length = image.width
+        else:
+            length = image.height
+        image = print_cards.cover_corners(image, length)
+
+        # Remove art credit. This is optional, and to my knowledge this is only
+        # applicable for cards from the Legacy of the Force: Smugglers
+        # expansion
+        image = print_cards.remove_art_credit(image)
+
+        # Add bleed to the edges of the card. The default bleed amount is
+        # 1/8 inch.
+        image = print_cards.add_bleed(image, 0.125)
+
+        # Rotate images so they are in portrait
+        image = print_cards.set_portrait(image)
+
+        # Save modified image. I save as .png to avoid generation loss.
+        save_name = file[:file.rfind('.')] + '.png'
+        image.save(os.path.join(output_directory, save_name))
